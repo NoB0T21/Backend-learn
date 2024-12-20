e0461fe00c423d2225fbc48d313f5c01dd65fabc
NoB0T21 <147228189+NoB0T21@users.noreply.github.com>
2024-12-20 13:01:58 +0530

    third commit in MP
    created secured route and created profile page also added posts feature to show created posts by user

--- Data Association/app.js
+++ Data Association/app.js
@@ -11,6 +11,7 @@ app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req,res) => {
     res.render('index')
@@ -19,12 +20,30 @@ app.get('/', (req,res) => {
 app.get('/login', (req,res) => {
     res.render('login')
 })
+app.get('/profile', isLoggedIn, async (req,res) => {
+    let user = await userModel.findOne({email: req.user.email}).populate("posts");
+    res.render('profile', {user});
+})
+
+app.post('/post', isLoggedIn, async (req,res) => {
+    let user = await userModel.findOne({email: req.user.email})
+    let {content} = req.body;
+
+    let post = await postModel.create({
+        user: user._id,
+        content
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile")
+})
 
 app.post('/register', async (req,res) => {
     let {username, name, age, password, email} = req.body;
 
     let user = await userModel.findOne({email});
-    if(user) return res.status(500).send("user already exisest");
+    if(user) return res.status(500).redirect("/login");
 
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(password, salt, async(err, hash) => {
@@ -38,10 +57,11 @@ app.post('/register', async (req,res) => {
 
             let token = jwt.sign({email: email, userid: user._id}, "shhhhh");
             res.cookie("token", token);
-            res.send("registered");
+            res.redirect("/profile");
         });
     })
 })
+
 
 app.post('/login', async (req,res) => {
     let {password, email} = req.body;
@@ -53,7 +73,7 @@ app.post('/login', async (req,res) => {
         if(result){
             let token = jwt.sign({email: email, userid: user._id}, "shhhhh");
             res.cookie("token", token);
-            res.status(200).send("you can login");
+            res.status(200).redirect("/profile");
         }
         else res.redirect("/login");
     })
@@ -64,10 +84,10 @@ app.get('/logout', (req,res) => {
     res.redirect('/login')
 });
 
-function isloggedIn(req, res, next){
-    if(req.cookie.token === "") res.send("you must be logged in");
+function isLoggedIn(req, res, next){
+    if(req.cookies.token === "") res.send("you must be logged in");
     else {
-        let data = jwt.verify(req.cookie.token, "shhhhh");
+        let data = jwt.verify(req.cookies.token, "shhhhh");
         req.user = data;
         next();
     }
