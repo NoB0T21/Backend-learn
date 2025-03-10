6664398e0a2a836d24b2bbda38866cd7cee39082
NoB0T21 <147228189+NoB0T21@users.noreply.github.com>
2025-03-10 18:00:47 +0530

    u

--- Basic_Backend/app.js
+++ Basic_Backend/app.js
@@ -16,6 +16,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get('/home', (req, res) => {
+    res.redirect("home");
+});
+
 app.use('/', homeRouter);
 app.use('/read', readRouter);
 app.use('/file', fileRouter);
--- Basic_Backend/routers/home.routes.js
+++ Basic_Backend/routers/home.routes.js
@@ -4,7 +4,7 @@ const {body} = require('express-validator');
 
 const homeController = require('../controllers/home.controller')
 
-router.get('/', homeController.showfile);
+router.get('/home', homeController.showfile);
 
 router.post('/create',
     body('filename').isLength({min: 3}).withMessage('Must be in 3 characters long'),
