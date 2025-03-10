ea4c520d89dc33979781eb1289923725848d0363
NoB0T21 <147228189+NoB0T21@users.noreply.github.com>
2025-03-10 15:00:39 +0530

    u

--- DRIVE/db/supabase.js
+++ DRIVE/db/supabase.js
@@ -1,6 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://kktujjbosxvzrswnbwpz.supabase.co';
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
